e8cb5a99b0344d66bc2c7d18db413306cec10169
AnshumanRawat10 <ar31jan2006@gmail.com>
2026-04-17 22:06:21 +0530

    Day 10 - Simple Log Analyzer

---
+++ pyday10.py
@@ -0,0 +1,41 @@
+# Given Logs
+logs = [
+    "ERROR DISK FULL",
+    "INFO STARTED",
+    "ERROR FILE MISSING",
+    "WARNING MEMORY LOW"
+]
+
+# Initialize counters
+error_count = 0
+info_count = 0
+warning_count = 0
+
+# Loop through logs (case-insensitive)
+for log in logs:
+    log = log.upper()
+    
+    if "ERROR" in log:
+        error_count += 1
+    elif "INFO" in log:
+        info_count += 1
+    elif "WARNING" in log:
+        warning_count += 1
+
+# Store counts in dictionary
+log_counts = {
+    "ERROR": error_count,
+    "INFO": info_count,
+    "WARNING": warning_count
+}
+
+# Find most frequent log type
+most_frequent = max(log_counts, key=log_counts.get)
+
+# Output
+print("\n📊 Log Analysis Results:\n")
+print("ERROR:", error_count)
+print("INFO:", info_count)
+print("WARNING:", warning_count)
+
+print("\n🔥 Most Frequent Log Type:", most_frequent)
